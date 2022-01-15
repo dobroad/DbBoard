513ecbcc88c803c40e90b60e1d5bc07642e5ed53
dobroad <98mengdo.gmail.com>
2022-01-15 21:03:10 +0900

    JDBC

---
+++ db.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE text_board;
+USE text_board;
+
+CREATE TABLE article(
+    id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(100) NOT NULL,
+    `body` TEXT NOT NULL
+);
+
+DESC article;
