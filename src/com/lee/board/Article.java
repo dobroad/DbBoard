7711ab3c1ee20818d9f026d2d87c66d93857b256
dobroad <98mengdo.gmail.com>
2022-01-17 11:01:24 +0900

    1.16

--- src/com/lee/board/Article.java
+++ src/com/lee/board/Article.java
@@ -1,13 +1,28 @@
 package com.lee.board;
 
-public class Article {
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public class Article { // 운반전용
 	public int id;
+	public LocalDateTime regDate;
+	public LocalDateTime updateDate;
 	public String title;
 	public String body;	
 	
-	public Article(int id, String title, String body) {
+	public Article(int id, LocalDateTime regDate, LocalDateTime updateDate, String title, String body) {
 		this.id = id;
+		this.regDate = regDate;
+		this.updateDate = updateDate;
 		this.title = title;
 		this.body = body;
+	}
+
+	public Article(Map<String, Object> articleMap) { // Article 객체에 담을 틀
+		this.id = (int)articleMap.get("id");
+		this.regDate = (LocalDateTime)articleMap.get("regDate");
+		this.updateDate = (LocalDateTime)articleMap.get("updateDate");
+		this.title = (String)articleMap.get("title");
+		this.body = (String)articleMap.get("body");
 	}
 }
